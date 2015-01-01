2fa381c02aae18d0d383417b87417e304823f2ef
Fadil Eledath <fadilfaizal@gmail.com>
2015-01-01 14:18:22 +0000

    Initial changes

--- header.php
+++ header.php
@@ -13,24 +13,58 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-
+<script src="//code.jquery.com/jquery-1.11.2.min.js"></script>
+<link href="//maxcdn.bootstrapcdn.com/bootswatch/3.3.1/yeti/bootstrap.min.css" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Roboto:100|Open+Sans" rel="stylesheet">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+<?php if(is_home() && $_SERVER['REQUEST_METHOD'] != "POST"): ?>
+	<style>
+	footer .site-info {
+		position: absolute;
+		bottom: 0px;
+		background: rgba(0, 0, 0, 0.5);
+	}
+	.navbar-default, .navbar-default .navbar-nav>.active>a, .navbar-default .navbar-nav>.active>a:hover, .navbar-default .navbar-nav> .active>a:focus, .navbar-default .navbar-nav>.active>a:hover {
+		background: rgba(0, 0, 0, 0.5) !important;
+	}
+	</style>
+	<?php else: ?>
+	<style>
+	#primary {
+		padding: 0 30px;
+	}
+	</style>
+<?php endif; ?>
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'iridescence' ); ?></a>
-
 	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div><!-- .site-branding -->
+         <nav class="navbar navbar-default" role="navigation">
+          <div class="container-fluid">
+           <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+             <span class="sr-only">Toggle navigation</span>
+             <span class="icon-bar"></span>
+             <span class="icon-bar"></span>
+             <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo("name"); ?></a>
+           </div>
 
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'iridescence' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
+           <div class="collapse navbar-collapse">
+            <?php /* Primary navigation */
+               wp_nav_menu( array(
+               'menu' => 'top_menu',
+               'depth' => 2,
+               'container' => false,
+               'menu_class' => 'nav navbar-nav navbar-right',
+               'walker' => new wp_bootstrap_navwalker())
+             );
+            ?>
+           </div>
+         </nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
--- index.php
+++ index.php
@@ -19,6 +19,7 @@ get_header(); ?>
 		<?php if ( have_posts() ) : ?>
 
 			<?php /* Start the Loop */ ?>
+			<?php if ($_SERVER["REQUEST_METHOD"] == "POST"): ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php
@@ -30,9 +31,24 @@ get_header(); ?>
 				?>
 
 			<?php endwhile; ?>
-
 			<?php iridescence_paging_nav(); ?>
+			<?php else: ?>
+			<div id="home-row">
+			<form id="more" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="POST">
+			<div class="home-subrow">
+			<?php for ($i=0;$i<3;$i++) : the_post(); ?>
+				<a href="<?php the_permalink(); ?>" class="post-item">
+					<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+					<div style="background: url('<?php echo $image[0] ?>')"></div></a>
 
+			<?php if ($i == 1): ?>
+			</div><div id="subrow2" class="home-subrow">
+			<?php endif; ?>
+			<?php endfor; ?>
+			<button type="submit">+</button>
+			</div>
+		</form></div>
+		<?php endif; ?>			
 		<?php else : ?>
 
 			<?php get_template_part( 'content', 'none' ); ?>
@@ -42,5 +58,4 @@ get_header(); ?>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
